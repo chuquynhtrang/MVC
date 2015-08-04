0244ff1090416cb62b32c3030139eb9a6a2f95d3
chuquynhtrang <chuquynhtrang@gmail.com>
2015-08-04 09:55:55 +0700

    update

--- Controller/UserController.php
+++ Controller/UserController.php
@@ -4,10 +4,7 @@ class UserController extends BaseController
 {
     public function index()
     {
-        if(isset($_GET['search']))
-            $this->indexBase('users','UserModel','username','users','index.php?controller=UserController&action=index&search='.$_GET['search'].'&page=');
-        else
-            $this->indexBase('users','UserModel','username','users','index.php?controller=UserController&action=index&page=');
+        $this->indexBase('users','UserModel','username','users','index.php?controller=UserController&action=index&page=');
     }
 
     public function viewAddUser()
@@ -50,45 +47,5 @@ class UserController extends BaseController
 
         $this->edit('users','UserModel',['username'=>$username,'email'=>$email,'password'=>$password,'activate'=>$activate,'time_updated'=>$time_updated,'image'=>$image]);
         header('Location:index.php?controller=UserController&action=index&page=1');
-    }
-
-    public function sortId()
-    {
-        if($_GET['order']=='asc')
-            $this->sort('users','UserModel','users','id','index.php?controller=UserController&action=sortId&order=asc&page=');
-        else
-            $this->sort('users','UserModel','users','id','index.php?controller=UserController&action=sortId&order=desc&page=');
-    }
-
-    public function sortUsername()
-    {
-        if($_GET['order']=='asc')
-            $this->sort('users','UserModel','users','username','index.php?controller=UserController&action=sortUsername&order=asc&page=');
-        else
-            $this->sort('users','UserModel','users','username','index.php?controller=UserController&action=sortUsername&order=desc&page=');
-    }
-
-    public function sortActivate()
-    {
-        if($_GET['order']=='asc')
-            $this->sort('users','UserModel','users','activate','index.php?controller=UserController&action=sortActivate&order=asc&page=');
-        else
-            $this->sort('users','UserModel','users','activate','index.php?controller=UserController&action=sortActivate&order=desc&page=');
-    }
-
-    public function sortTimeCreated()
-    {
-        if($_GET['order']=='asc')
-            $this->sort('users','UserModel','users','time_created','index.php?controller=UserController&action=sortTimeCreated&order=asc&page=');
-        else
-            $this->sort('users','UserModel','users','time_created','index.php?controller=UserController&action=sortTimeCreated&order=desc&page=');
-    }
-
-    public function sortTimeUpdated()
-    {
-        if($_GET['order']=='asc')
-            $this->sort('users','UserModel','users','time_updated','index.php?controller=UserController&action=sortTimeUpdated&order=asc&page=');
-        else
-            $this->sort('users','UserModel','users','time_updated','index.php?controller=UserController&action=sortTimeUpdated&order=desc&page=');
     }
 }
