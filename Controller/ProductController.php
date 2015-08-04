7398aa3f89e321deaebdf69b262f923f987b0a07
chuquynhtrang <chuquynhtrang@gmail.com>
2015-08-04 09:56:50 +0700

    update

--- Controller/ProductController.php
+++ Controller/ProductController.php
@@ -4,10 +4,7 @@ class ProductController extends BaseController
 {
     public function index()
     {
-        if(isset($_GET['search']))
-            $this->indexBase('products','ProductModel','productname','products','index.php?controller=ProductController&action=index&search='.$_GET['search'].'&page=');
-        else
-            $this->indexBase('products','ProductModel','productname','products','index.php?controller=ProductController&action=index&page=');
+        $this->indexBase('products','ProductModel','productname','products','index.php?controller=ProductController&action=index&page=');
     }
 
     public function viewAddProduct()
@@ -50,45 +47,5 @@ class ProductController extends BaseController
 
         $this->edit('products','ProductModel',['productname'=>$productname,'price'=>$price,'description'=>$description,'activate'=>$activate,'time_updated'=>$time_updated,'image'=>$image]);
         header('Location:index.php?controller=ProductController&action=index&page=1');
-    }
-
-    public function sortId()
-    {
-        if($_GET['order']=='asc')
-            $this->sort('products','ProductModel','products','id','index.php?controller=ProductController&action=sortId&order=asc&page=');
-        else
-            $this->sort('products','ProductModel','products','id','index.php?controller=ProductController&action=sortId&order=desc&page=');
-    }
-
-    public function sortProductname()
-    {
-        if($_GET['order']=='asc')
-            $this->sort('products','ProductModel','products','productname','index.php?controller=ProductController&action=sortProductname&order=asc&page=');
-        else
-            $this->sort('products','ProductModel','products','productname','index.php?controller=ProductController&action=sortProductname&order=desc&page=');
-    }
-
-    public function sortActivate()
-    {
-        if($_GET['order']=='asc')
-            $this->sort('products','ProductModel','products','activate','index.php?controller=ProductController&action=sortActivate&order=asc&page=');
-        else
-            $this->sort('products','ProductModel','products','activate','index.php?controller=ProductController&action=sortActivate&order=desc&page=');
-    }
-
-    public function sortTimeCreated()
-    {
-        if($_GET['order']=='asc')
-            $this->sort('products','ProductModel','products','time_created','index.php?controller=ProductController&action=sortTimeCreated&order=asc&page=');
-        else
-            $this->sort('products','ProductModel','products','time_created','index.php?controller=ProductController&action=sortTimeCreated&order=desc&page=');
-    }
-
-    public function sortTimeUpdated()
-    {
-        if($_GET['order']=='asc')
-            $this->sort('products','ProductModel','products','time_updated','index.php?controller=ProductController&action=sortTimeUpdated&order=asc&page=');
-        else
-            $this->sort('products','ProductModel','products','time_updated','index.php?controller=ProductController&action=sortTimeUpdated&order=desc&page=');
     }
 }
